e52065c65650e9dd391b6b49e0422f0443ba765a
DevMahdiTr <trabelsimahdi79@gmail.com>
2026-05-30 22:43:57 +0100

    first push

---
+++ src/test/java/com/iteam/buget/BugetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iteam.buget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
